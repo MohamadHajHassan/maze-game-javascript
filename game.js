c2798b8486e0aa4e83745e26aebee29256e175d2
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2022-09-04 17:59:46 +0300

    added functions implementation

--- game.js
+++ game.js
@@ -11,4 +11,34 @@ document.addEventListener("DOMContentLoaded", function () {
   start.addEventListener("mouseover", resetBordersColor);
   boundaries.forEach((item) => item.addEventListener("mouseover", lostRound));
   end.addEventListener("mouseover", winRound);
+
+  // Functions
+  function resetBordersColor() {
+    boundaries.forEach((item) => {
+      item.style.border = "1px solid black";
+      status.innerHTML =
+        'Try to reach the end "E" without touching the borders.';
+    });
+    canPlay = true;
+  }
+
+  function winRound() {
+    if (canPlay) {
+      boundaries.forEach((item) => {
+        item.style.border = "1px solid green";
+      });
+      status.innerHTML =
+        'You win! Move your mouse over the "S" to play another round.';
+      canPlay = false;
+    }
+  }
+
+  function lostRound() {
+    if (canPlay) {
+      boundaries.forEach((i) => (i.style.border = "1px solid red"));
+      status.innerHTML =
+        'You lost! Move your mouse over the "S" to play another round.';
+      canPlay = false;
+    }
+  }
 });
