def6a964f7743c1f3361642e5fb15e50b0af8b09
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2022-09-04 17:55:20 +0300

    added variables

---
+++ game.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Declare variables
+  var start = document.getElementById("start");
+  var end = document.getElementById("end");
+  var boundaries = document.querySelectorAll(".boundary");
+  var status = document.getElementById("status");
+  var game = document.getElementById("game");
+  var canPlay = true;
+});
